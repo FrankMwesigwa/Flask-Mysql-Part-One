43eb0d5c1e53ae70fdea544bf6d7530575d30e86
FrankMwesigwa <frank.mwesigwa1@gmail.com>
2019-01-27 11:53:35 +0300

    initial setup of flask application

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-from src import app
+from flask import Flask # here we are importing the Flask class from the flask package 
+
+app = Flask(__name__) # here we are creating the app variable and setting it to an instance of the Flask class                            and passing in a special variable name in python which is just the name of the module
+
+@app.route("/") # here we are creating our routes. routes are what we type in our browser to go to different pages
+def hello():
+    return "<h1>Home Page !!</h1>"
+
+@app.route("/about") 
+def about():
+    return "<h1>About us Page !!</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug='True')
