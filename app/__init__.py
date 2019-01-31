2e8335cd619cf8f69cebf746df278640dbc1d23f
FrankMwesigwa <frank.mwesigwa1@gmail.com>
2019-02-01 00:33:41 +0300

    flask mega user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -11,6 +12,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
 
