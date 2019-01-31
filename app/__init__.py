53472cc7bfa6cc5bd71475d9f91a8826248868b2
FrankMwesigwa <frank.mwesigwa1@gmail.com>
2019-01-31 23:39:17 +0300

    flask mega database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "Flask@!123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://farmprod:Master@123Q@127.0.0.1/farmprod'
 
-from app import routes
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
 
 
