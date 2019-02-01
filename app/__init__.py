4bdb29af327b08d311e6d7c4a2a1ecb97d49501d
FrankMwesigwa <frank.mwesigwa1@gmail.com>
2019-02-01 12:03:57 +0300

    made some changes to user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
@@ -11,9 +12,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://farmprod:Master@123Q@12
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from app import routes, models
 
