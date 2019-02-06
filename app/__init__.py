f21baf8d1ecc412cb093429559afbb481d0d89ae
FrankMwesigwa <frank.mwesigwa1@gmail.com>
2019-02-06 14:24:12 +0300

    worked on the user registration form using blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://farmprod:Master@123Q@12
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+Bootstrap(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
@@ -19,6 +21,9 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-from app import routes, models
+from app import models
+from app.home import home
+from app.auth import auth
 
-
+app.register_blueprint(home)
+app.register_blueprint(auth)
--- app/models.py
+++ app/models.py
@@ -1,30 +1,46 @@
 from flask_login import UserMixin
-from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login_manager
-from flask_login import UserMixin
 
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+class Employee(UserMixin, db.Model):
+    """
+    Create an Employee table
+    """
 
-class User(db.Model, UserMixin):
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'employees'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String(128))
-    image_file = db.Column(db.String(20), nullable=False, default='logo.png')
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-    
+    email = db.Column(db.String(60), index=True, unique=True)
+    username = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Employee: {}>'.format(self.username)
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
